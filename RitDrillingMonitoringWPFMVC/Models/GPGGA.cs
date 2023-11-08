cdef133a0ea2f68f70bda4851bf53ae6f72f379b
Daniil Molotkov <molotkov.58@ya.ru>
2023-11-09 02:24:25 +0700

    feat: Add GPGGA parser

---
+++ RitDrillingMonitoringWPFMVC/Models/GPGGA.cs
@@ -0,0 +1,35 @@
+﻿using GMap.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RitDrillingMonitoringWPFMVC.Models
+{
+    public class GPGGA
+    {
+        public TimeSpan time;
+        public PointLatLng point;
+        public int solutionType;
+        public int usedSatellitesCount;
+        public double geometricFactor;
+        public double altitudeAboveSea;
+        public double heightOfGeoidAboveEllipsoid;
+        public bool isEmpty = true;
+
+        public GPGGA() { }
+
+        public GPGGA(TimeSpan time, PointLatLng point, int solutionType, int usedSatellitesCount, double geometricFactor, double altitudeAboveSea, double heightOfGeoidAboveEllipsoid)
+        {
+            this.time = time;
+            this.point = point;
+            this.solutionType = solutionType;
+            this.usedSatellitesCount = usedSatellitesCount;
+            this.geometricFactor = geometricFactor;
+            this.altitudeAboveSea = altitudeAboveSea;
+            this.heightOfGeoidAboveEllipsoid = heightOfGeoidAboveEllipsoid;
+            isEmpty = false;
+        }
+    }
+}
---
+++ RitDrillingMonitoringWPFMVC/Utils/NMEAParser.cs
@@ -0,0 +1,64 @@
+﻿using GMap.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using RitDrillingMonitoringWPFMVC.Models;
+
+namespace RitDrillingMonitoringWPFMVC.Utils
+{
+    public static class NMEAParser
+    {
+        static readonly Regex GpggaRegex = new(@"\$GPGGA,\d+\.*\d*,\d+\.\d+,(N|S),\d+\.\d+,(E|W),\d,\d+,\d+\.*\d*,\d+\.*\d*,M,\d+\.*\d*,M,\d*,.*");
+
+        public static GPGGA ParseGPGGA(string data)
+        {
+            Match match = GpggaRegex.Match(data);
+            if (!match.Success) return new GPGGA();
+            data = match.Value;
+            string[] parts = data.Split(',');
+            TimeSpan UTCTime = ParseUTCTime(parts[1]);
+            double latitude = ParseLatitude(parts[2], parts[3]);
+            double longtitude = ParseLongtitude(parts[4], parts[5]);
+            PointLatLng point = new(latitude, longtitude);
+            int solutionType = int.Parse(parts[6]);
+            int satellitesUsedCount = int.Parse(parts[7]);
+            double geometricFactor = double.Parse(parts[8]);
+            double altitudeAboveSea = double.Parse(parts[9]);
+            double heightOfGeoidAboveEllipsoid = double.Parse(parts[11]);
+
+            return new GPGGA(UTCTime, point, solutionType, satellitesUsedCount, geometricFactor, altitudeAboveSea, heightOfGeoidAboveEllipsoid);
+        }
+
+        private static TimeSpan ParseUTCTime(string part)
+        {
+            int hours = int.Parse(part.Substring(0, 2));
+            int minutes = int.Parse(part.Substring(2, 2));
+            int seconds = int.Parse(part.Substring(4, 2));
+            TimeSpan UTCtime = new(hours, minutes, seconds);
+            return UTCtime;
+        }
+
+        private static double ParseLatitude(string part, string side)
+        {
+            part = part.Replace(".", "");
+            part = part.Insert(2, ",");
+            double latitude = double.Parse(part);
+            if (side == "S")
+                latitude = -latitude;
+            return latitude;
+        }
+
+        private static double ParseLongtitude(string part, string side)
+        {
+            part = part.Replace(".", "");
+            part = part.Insert(3, ",");
+            double longtitude = double.Parse(part);
+            if (side == "W")
+                longtitude = -longtitude;
+            return longtitude;
+        }
+    }
+}
